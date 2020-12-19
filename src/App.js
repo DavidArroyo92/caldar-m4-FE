27c70fa9140aa18759756f703b85d757719af420
maxivgg <maxivgg@gmail.com>
2020-12-19 15:02:08 -0300

    fist commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,35 @@
-import React from 'react';
-import styles from './App.module.css';
-import Navbar from './layout/navbar/navbar';
-import Header from './layout/header/header';
-import Main from './layout/main/main';
-import Appointment from './components/appointment';
-import Boilers from './components/boilers';
-import BoilersTypes from './components/boilersTypes';
-import Buildings from './components/buildings';
-import Custumers from './components/custumers';
-import Technicians from './components/technicians';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-
+import React from "react";
+import styles from "./App.module.css";
+import Navbar from "./layout/navbar/navbar";
+import Header from "./layout/header/header";
+import Appointment from "./components/appointment";
+import Boilers from "./components/Boilers/Boilers";
+import BoilersTypes from "./components/boilersTypes";
+import Buildings from "./components/buildings";
+import Custumers from "./components/custumers";
+import Technicians from "./components/technicians";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-   <Router>
+    <Router>
       <div className="App">
         <div className={styles.wrapper}>
-        <Navbar/>
+          <Navbar />
           <div className={styles.mainContent}>
-            <Header/>
-            <Main/>
+            <Header />
+            <Switch>
+              <Route path="appoinment" component={Appointment} />
+              <Route path="/boilers" component={Boilers} />
+              <Route path="boilersTypes" component={BoilersTypes} />
+              <Route path="buildings" component={Buildings} />
+              <Route path="custumers" component={Custumers} />
+              <Route path="technicians" component={Technicians} />
+            </Switch>
           </div>
         </div>
-        <Switch>
-          <Route path="appoinment" component={Appointment}/>
-          <Route path="boilers" component={Boilers}/>
-          <Route path="boilersTypes" component={BoilersTypes}/>
-          <Route path="buildings" component={Buildings}/>
-          <Route path="custumers" component={Custumers}/>
-          <Route path="technicians" component={Technicians} />
-        </Switch>
       </div>
-   </Router>
+    </Router>
   );
 }
 
